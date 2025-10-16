124a1b93c5d8b8f5a2b670b348f6a3987b8f4b59
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2025-10-16 09:59:53 -0500

    sremana3

---
+++ modulo2/javascript/02_variables.js
@@ -0,0 +1,52 @@
+console.log("VARIABLES")
+
+saludo="Hola utilizando variables "
+console.log(" Variable Saludo" , saludos);
+console.log("Tipo de variable", typeof(saludo));
+
+
+numero=256
+console.log(" Variable Numero " , numero );
+console.log("Tipo de variable", typeof(numero ));
+
+
+booleana=True 
+console.log(" Variable Saludo" , booleana);
+console.log("Tipo de variable", typeof(booleana));
+
+
+
+indefinida= undefined
+console.log(" Variable Saludo" , indefinida );
+console.log("Tipo de variable", typeof(indefinida ));
+
+
+
+nula = null
+console.log(" Variable Saludo" , nula);
+console.log("Tipo de variable", typeof(nula));
+
+
+
+
+bigentero=8765675675785463654n
+console.log(" Variable Saludo" , bigentero);
+console.log("Tipo de variable", typeof(bigentero));
+
+objeto={
+    nombre: "Dayana", 
+    edad:30
+}
+
+console.log(" Variable Saludo" , persona);
+console.log("Tipo de variable", typeof(persona));
+
+
+numeros= [12,12,22,34,4,4,]
+console.log(" Variable Saludo" , numeros);
+console.log("Tipo de variable", typeof(numeros));
+
+
+
+
+
