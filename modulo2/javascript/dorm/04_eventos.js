edf75013cf83c8385813dd97730a906cf3301aff
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2025-10-28 09:58:55 -0500

    clase 2025-10-28

--- modulo2/javascript/dorm/04_eventos.js
+++ modulo2/javascript/dorm/04_eventos.js
@@ -8,3 +8,18 @@ document.getElementById('crear').addEventListener('click', ()=>{
 document.getElementById('eliminar').addEventListener('click', ()=>{
     if(parrafo) parrafo.remove();
 });
+
+function saludar(){
+    alert ("hola desde la funcion");
+}
+
+document.getElementById('parrafo')
+    .addEventListener('dblclick', ()=>{
+    alert("Se hizo doble click en el parrafo")
+});
+
+document.getElementById('nombre')
+    .addEventListener('input', (e)=>{
+    console.log("Escribiendo", e.target.value)
+})
+
