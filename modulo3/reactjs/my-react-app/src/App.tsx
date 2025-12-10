d4d6686f680363192ecd9070daa6fb76f6756191
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2025-12-10 10:52:15 -0600

    Segundo corte

---
+++ modulo3/reactjs/my-react-app/src/App.tsx
@@ -0,0 +1,95 @@
+/*
+import HelloWorld from './components/HelloWorld';
+import WelcomeUser from './components/WelcomeUser';
+import ButtonCounter from './components/ButtonCounter';
+import TextInput from './components/TextInput';
+import ToggleMessage from './components/ToggleMessage';
+import ShowDate from './components/ShowDate';
+import ColorBox from './components/ColorBox';
+import ConditionalText from './components/ConditionalText';
+import EmojiList from './components/EmojiList';
+import SimpleForm from './components/SimpleForm';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+      <WelcomeUser />
+      <ButtonCounter />
+      <TextInput />
+      <ToggleMessage />
+      <ShowDate />
+      <ColorBox />
+      <ConditionalText />
+      <EmojiList />
+      <SimpleForm />
+    </>
+  );
+}
+
+export default App; */
+
+/*
+import Greeting from './Props/Greeting';
+import UserCard from './Props/UseCard';
+import PromedioTresNotasForm from './components/PromedioTresNotasForm';
+
+function App() {
+
+  const userData = { name: 'Alexander', age: 30, address: 'Calle Falsa 123', phone: '1234567890' };
+  return (
+    <>
+      <Greeting name="Alexander" />
+      <Greeting name="Axel" />
+      <PromedioTresNotasForm />
+      <UserCard user={userData} />
+    </>
+  );
+}
+
+export default App;
+*/
+
+/*
+import ParentButtonOne from './Props/ParentButtonOne';
+
+function App() {
+  return (
+    <>
+      <ParentButtonOne />
+    </>
+  );
+}
+export default App;
+*/
+
+/*
+import ParentIncrementButton from './Props/ParentIncrementButton';
+
+function App() {
+  return (
+    <>
+      <ParentIncrementButton />
+    </>
+  );
+}
+export default App;
+*/
+
+import ParentInputNotifier from './Props/ParentInputNotifier';
+import ParentUserList from './Props/ParentUserList';
+
+import ParentLoginForm from './Props/ParentLoginForm';
+
+function App() {
+  return (
+    <>
+      <ParentInputNotifier />
+      <ParentUserList />
+      <ParentLoginForm />
+    </>
+  );
+}
+export default App;
+
+
